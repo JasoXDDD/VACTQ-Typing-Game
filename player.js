5d6647f42949513b714353270b922ff16f67371b
BobTheFarmer <colinmweis@gmail.com>
2023-10-18 10:53:07 -0700

    Player object

---
+++ player.js
@@ -0,0 +1,19 @@
+const stepAmount = 5;
+
+class Player {
+    current_progress;
+    constructor(index) {
+        this.progress = 0;
+        this.index = index;
+    }
+    
+    reset() {
+        this.progress = 0;
+    }
+
+    update(positionData) {
+        this.progress = positionData[this.index];
+    }
+}
+
+
